60373c7be62e55f6467654ab89a8b2314d92a7bb
SavosinSergey <SavosinSergey@SERGEYPC>
2017-03-06 22:44:35 +0300

    Ninja MVC project added

---
+++ MvcNinjaApp/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MvcNinjaApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequets()
+        {
+            // custom CultureInfo for editing decimals in Price fields
+            var newCulture = (CultureInfo)CultureInfo.CreateSpecificCulture("En-us");// .CurrentCulture.Clone();
+            newCulture.NumberFormat.NumberGroupSeparator = "~";
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = newCulture;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = newCulture;
+
+        }
+    }
+}
---
+++ MvcNinjaApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcNinjaApp.Startup))]
+namespace MvcNinjaApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
